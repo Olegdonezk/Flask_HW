84afe7b7594f1e1aedcd1d33489de94ce4dc6a82
Oleg <olegdonezk@gmail.com>
2026-04-15 01:02:16 +0200

    Flask HW1

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return 'Hello, Flask!'
+
+@app.route('/user/<Oleg>')
+def user(Oleg):
+    return f'Hello, {Oleg}!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
